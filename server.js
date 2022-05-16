b9847dac14c9796eee84f5263f85ac989731ab95
nbross <nbross2001@gmail.com>
2022-05-16 16:40:31 -0400

    changed tracker menu to work as a function now, added view departments function and quit function, terminal is now outputing data from database

--- server.js
+++ server.js
@@ -3,28 +3,35 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql2');
 const db = require('./db/connection');
+const { exit } = require('process');
 require('console.table');
 
 
+db.connect(async function () {
+    trackerMenu();
+})
 
-const trackerMenu = () => {
-    return inquirer.prompt([
+function trackerMenu() {
+    inquirer.prompt([
         {
             type: 'list',
-            name: 'tracker menu',
+            name: 'choice',
             message: 'Welcome to the Employee Tracker Menu. Please select one of the options you would like to do?',
-            choices: ['View Company Departments', 'View Company Roles', 'View Company Employees', 'Add a New Company Department', 'Add a New Company Role', 'Add a New Company Employee', 'Update an Existing Employee Role', 'Quit']
+            choices: [
+                'View Company Departments', 'View Company Roles', 'View Company Employees', 'Add a New Company Department', 'Add a New Company Role', 'Add a New Company Employee', 'Quit'
+            ],
         }])
-        .then(userMenuChoice => {
-            switch (userMenuChoice.trackerMenu) {
+        .then((answer) => {
+            switch (answer.choice) {
+                
                 case 'View Company Departments':
-                    viewDepartments();
+                    viewCompanyDepartments();
                     break;
                 case 'View Company Roles':
-                    viewRoles();
+                    viewCompanyRoles();
                     break;
                 case 'View Company Employees':
-                    viewEmployees();
+                    viewCompanyEmployees();
                     break;
                 case 'Add a New Company Department':
                     addCompanyDepartment();
@@ -35,13 +42,26 @@ const trackerMenu = () => {
                 case 'Add a New Company Employee':
                     addCompanyEmployee();
                     break;
-                case 'Update an Existing Employee Role':
-                    updateRole();
+                case 'Quit':
+                    Quit();
                     break;
-                default:
-                    process.exit();
             }
-        });
+        })
 };
 
-trackerMenu();
+function viewCompanyDepartments() {
+    const request = "SELECT * FROM department";
+    db.query(request, function (err, res) {
+        if (err) throw err;
+        console.log("Viewing All Company Departments");
+        console.table(res);
+        trackerMenu();
+    })
+};
+
+function Quit() {
+    console.log('Thanks for using the employee tracker!'); 
+    console.log('The company thanks you for your time!');
+    console.log('Goodbye!');
+    process.exit();
+}
