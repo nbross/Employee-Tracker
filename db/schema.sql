d01bd665851850f3b2a422dec5d6548c7a081707
nbross <nbross2001@gmail.com>
2022-05-15 20:30:02 -0400

    schema db created and connection to db secured

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id INT UNSIGNED NULL
+);
